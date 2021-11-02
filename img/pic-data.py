2a01649841ef9672ddeb73747fa0f206611a9abc
Sergey Tkachenko <sergeyprujina@gmail.com>
2021-11-02 17:32:29 +0100

    added python img to matrix 1d script

---
+++ img/pic-data.py
@@ -0,0 +1,19 @@
+import numpy as np
+from PIL import Image
+
+Nx = Ny = 30
+image = Image.open('wiki.jpg')
+image = image.resize((Nx,Ny))
+image = image.convert('L')
+data = np.asarray(image)
+
+threshold = 50
+matrix = (data < threshold).astype(dtype=np.uint8)
+image_test = Image.fromarray(matrix*255)
+image_test.save('image-test.jpg')
+
+with open('file.txt', 'w') as f:
+    for i in range(Nx):
+        for j in range(Ny):
+            f.write(str(matrix[i, j]))
+            f.write('\n')
