e4b3cbc872313c1fc9e409792d61d5d71672b272
Sergey Tkachenko <sergeyprujina@gmail.com>
2024-03-07 16:32:43 +0100

    feat: add cross-sections to the Solution class

--- plot/extsgnplot.py
+++ plot/extsgnplot.py
@@ -19,27 +19,37 @@ class Solution:
     def __init__(self, file):
         with open(file) as f:
             lines = [line.strip() for line in f.readlines()]
-            x = np.array([float(line.split()[0]) for line in lines])
-            y = np.array([float(line.split()[1]) for line in lines])
-            h = np.array([float(line.split()[2]) for line in lines])
-            u = np.array([float(line.split()[3]) for line in lines])
-            v = np.array([float(line.split()[4]) for line in lines])
-            eta = np.array([float(line.split()[5]) for line in lines])
-            w = np.array([float(line.split()[6]) for line in lines])
 
-            self.x = x.reshape((n_x, n_y))
-            self.y = y.reshape((n_x, n_y))
-            self.h = h.reshape((n_x, n_y))
-            self.u = u.reshape((n_x, n_y))
-            self.v = v.reshape((n_x, n_y))
-            self.eta = eta.reshape((n_x, n_y))
-            self.w = w.reshape((n_x, n_y))
-            self.t = float(lines[0].split()[7])
+        x = np.array([float(line.split()[0]) for line in lines])
+        y = np.array([float(line.split()[1]) for line in lines])
+        h = np.array([float(line.split()[2]) for line in lines])
+        u = np.array([float(line.split()[3]) for line in lines])
+        v = np.array([float(line.split()[4]) for line in lines])
+        eta = np.array([float(line.split()[5]) for line in lines])
+        w = np.array([float(line.split()[6]) for line in lines])
 
-            grad_h = np.gradient(self.h)
-            grad_h_abs = np.sqrt(grad_h[0]**2 + grad_h[1]**2)
-            schlieren = np.log(1 + np.log(1 + 25 * grad_h_abs))
-            self.schlieren = schlieren
+        self.x = x.reshape((n_x, n_y))
+        self.y = y.reshape((n_x, n_y))
+        self.h = h.reshape((n_x, n_y))
+        self.u = u.reshape((n_x, n_y))
+        self.v = v.reshape((n_x, n_y))
+        self.eta = eta.reshape((n_x, n_y))
+        self.w = w.reshape((n_x, n_y))
+        self.t = float(lines[0].split()[7])
+
+        mid_index = n_y // 2 - 1
+
+        self.section_x = self.x[:, mid_index]
+        self.section_h = self.h[:, mid_index]
+        self.section_u = self.u[:, mid_index]
+        self.section_v = self.v[:, mid_index]
+        self.section_eta = self.eta[:, mid_index]
+        self.section_w = self.w[:, mid_index]
+
+        grad_h = np.gradient(self.h)
+        grad_h_abs = np.sqrt(grad_h[0] ** 2 + grad_h[1] ** 2)
+        schlieren = np.log(1 + np.log(1 + 25 * grad_h_abs))
+        self.schlieren = schlieren
 
     def plot_schlieren(self, file):
         fig, ax = plt.subplots()
