6427b01741e7428dd239177eef96dd23cf9a66c1
Sergey Tkachenko <sergeyprujina@gmail.com>
2024-03-12 12:50:39 +0100

    feat: truncate colormap and rotate the image

--- plot/xsgnplot.py
+++ plot/xsgnplot.py
@@ -1,9 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from matplotlib.colors import LinearSegmentedColormap
 
-n_x = 100
-n_y = 100
+n_x = 500
+n_y = 500
+
+
+def truncate_colormap(cmap_name, cutoff_percentage=0.8):
+    original_cmap = plt.get_cmap(cmap_name)
+    num_colors = original_cmap.N
+    cutoff_index = int(num_colors * cutoff_percentage)
+    new_colors = original_cmap(np.linspace(0, cutoff_index / num_colors,
+                                           cutoff_index))
+    truncated_cmap_name = f'truncated_{cmap_name}'
+    truncated_cmap = LinearSegmentedColormap.from_list(truncated_cmap_name,
+                                                       new_colors)
+
+    return truncated_cmap
 
 
 def create_testdata(file, nx=10, ny=5):
@@ -49,6 +63,7 @@ class Solution:
         grad_h = np.gradient(self.h)
         grad_h_abs = np.sqrt(grad_h[0] ** 2 + grad_h[1] ** 2)
         schlieren = np.log(1 + np.log(1 + 25 * grad_h_abs))
+        self.grad_h_abs = grad_h_abs
         self.schlieren = schlieren
 
     def plot_schlieren(self, file):
@@ -58,6 +73,18 @@ class Solution:
         ax.contourf(self.x, self.y, self.schlieren, levels=100,
                     cmap=plt.get_cmap('Blues'))
 
+        plt.savefig(file, dpi=1200)
+        plt.close()
+
+    def plot_artsy(self, file, cmap='bone_r'):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(5, 5)
+
+        ax.contourf(self.x, -self.y, self.h.T, levels=100,
+                    cmap=plt.get_cmap(cmap))
+
+        ax.axis('off')
+        # fig.patch.set_alpha(0)
         plt.savefig(file, dpi=1200)
         plt.close()
 
@@ -101,7 +128,11 @@ class Solution:
 if __name__ == '__main__':
     file = 'out/res.dat'
     png = 'img/schlieren-2d.png'
+    png_artsy = 'img/artsy.png'
     png_1d = 'img/huvetaw-1d.png'
     solution = Solution(file)
-    solution.plot_schlieren(png)
-    solution.plot_sections(png_1d)
+
+    cmap = truncate_colormap('ocean_r', cutoff_percentage=0.7)
+    solution.plot_artsy(png_artsy, cmap=cmap)
+    # solution.plot_schlieren(png)
+    # solution.plot_sections(png_1d)
